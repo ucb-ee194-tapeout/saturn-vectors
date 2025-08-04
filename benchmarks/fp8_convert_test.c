b188be87f758db5e5a017689ff75aad0997ca63e
Rakanic <nicorakela@berkeley.edu>
2025-08-03 23:55:58 -0700

    fully working widening to fp16

--- benchmarks/fp8_convert_test.c
+++ benchmarks/fp8_convert_test.c
@@ -3,7 +3,8 @@
 #include <stdio.h>
 
 int main(void) {
-    uint8_t input[16] = {0, 56, 184, 48, 176, 60, 193, 70, 157, 120, 0, 120, 120, 248, 121, 0};
+    uint8_t input1[8] = {0, 1, 4, 126, 127, 128, 240, 247};
+    uint8_t input2[8] = {248, 255, 42, 170, 85, 15, 143};
     float   output[16];
     
         asm volatile (
@@ -31,7 +32,39 @@ int main(void) {
         "vsetvli    t0, t0, e32, m1, tu, mu\n"
         "vse32.v    v6,     (a1)          \n"
         :
-        : [IN] "r"(input), [OUT] "r"(output)
+        : [IN] "r"(input1), [OUT] "r"(output)
+        : "a0", "a1", "t0",
+          /* declaramos los registros vectoriales que tocamos */
+          "v2", "v4", "v6",
+          "memory"
+    );
+
+        asm volatile (
+        /* a0 = &input, a1 = &output */
+        "mv         a0,     %[IN]\n"
+        "mv         a1,     %[OUT]\n"
+
+        /* --- paso 1: VL=16, E8 --- */
+        "li         t0,     16\n"
+        "vsetvli    t0, t0, e8,  m1, tu, mu\n"
+        "vle8.v     v2,     (a0)          \n"   /* v2 <- uint8 */
+
+        /* --- paso 2: E8 -> E16 --- */
+        "vfwcvt.f.f.v  v4, v2            \n"   /* v4: f16 */
+
+        /* configurar vtype a E16 antes de la 2.ª ampliación           */
+        "li         t0,     16\n"
+        "vsetvli    t0, t0, e16, m1, tu, mu\n"
+
+        /* --- paso 3: E16 -> E32 --- */
+        "vfwcvt.f.f.v  v6, v4            \n"   /* v6: f32 */
+
+        /* --- paso 4: guardar como 32-bit fp --- */
+        "li         t0,     16\n"
+        "vsetvli    t0, t0, e32, m1, tu, mu\n"
+        "vse32.v    v6,     (a1)          \n"
+        :
+        : [IN] "r"(input2), [OUT] "r"(output+8)
         : "a0", "a1", "t0",
           /* declaramos los registros vectoriales que tocamos */
           "v2", "v4", "v6",
@@ -40,7 +73,7 @@ int main(void) {
 
     /* --- comprobar resultado --- */
     for (int i = 0; i < 16; ++i)
-        printf("out[%d] = %d \n", i, (int) (output[i]*10));
+        printf("out[%d] = %d \n", i, (int) (output[i]*10000));
 
     return 0;
 
